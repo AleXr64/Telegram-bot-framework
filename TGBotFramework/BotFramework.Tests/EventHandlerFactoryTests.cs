5150e5b1501ac58343bf901a4499725d325563db
AleXr64 <alex91_@mail.ru>
2020-02-16 03:55:40 +0300

    Cleanup EventHandlerFactory test

--- TGBotFramework/BotFramework.Tests/EventHandlerFactoryTests.cs
+++ TGBotFramework/BotFramework.Tests/EventHandlerFactoryTests.cs
@@ -1,6 +1,6 @@
-﻿using System.Threading.Tasks;
-using BotFramework.Attributes;
+﻿using BotFramework.Attributes;
 using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
 using Telegram.Bot.Types;
 using Xunit;
 
@@ -22,6 +22,31 @@ namespace BotFramework.Tests
             _factory = new EventHandlerFactory(scopeFactory);
         }
 
+        private class TestProxy
+        {
+            public bool Acessed;
+            public bool Executed;
+        }
+
+        private class TestHandlerAttribute: HandlerAttribute
+        {
+            protected override bool CanHandle(HandlerParams param) { return true; }
+        }
+
+        private class TestHandler: BotEventHandler
+        {
+            private readonly TestProxy _proxy;
+
+            public TestHandler(TestProxy proxy)
+            {
+                _proxy = proxy;
+                _proxy.Acessed = true;
+            }
+
+            [TestHandler]
+            public void Fact() { _proxy.Executed = true; }
+        }
+
         [Fact]
         public async Task Test()
         {
@@ -32,30 +57,5 @@ namespace BotFramework.Tests
             Assert.True(_proxy.Acessed);
             Assert.True(_proxy.Executed);
         }
-    }
-
-    public class TestProxy
-    {
-        public bool Acessed;
-        public bool Executed;
-    }
-
-    public class TestHandlerAttribute: HandlerAttribute
-    {
-        protected override bool CanHandle(HandlerParams param) { return true; }
-    }
-
-    public class TestHandler: BotEventHandler
-    {
-        private readonly TestProxy _proxy;
-
-        public TestHandler(TestProxy proxy)
-        {
-            _proxy = proxy;
-            _proxy.Acessed = true;
-        }
-
-        [TestHandler]
-        public void Fact() { _proxy.Executed = true; }
     }
 }
