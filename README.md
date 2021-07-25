be6e5370d62c12e6371bfdd01d050ba5c8dceaae
voed <voed@i.ua>
2021-07-26 02:21:47 +0300

    Add example project (#56)
    * Add example project
    
    * Update README.md
    
    * Cleanup
    
    * Use package instead lib

--- README.md
+++ README.md
@@ -3,23 +3,38 @@ Framework to simplify writing telegram bots.
 Register framework as service in DI conatiner:
 
     services.AddTelegramBot();
-Inherit from the BotEventHandler class:
+Inherit from the BotEventHandler class and add new method with specified attributes:
 
+```csharp
     public class EventHandler:BotEventHandler
     {
-    ...
-Add new method, mark that with Attribute:
+        // Answer on "/start" command from private messages
+        [Command(InChat.Private, "start")]
+        public async Task Start() => await Bot.SendTextMessageAsync(Chat, "Hello! U started me =)");
 
-    [Command("start")]
-    public async Task Start() => await Bot.SendTextMessageAsync(Chat, "Hello! U started me =)");
+        //Answer on message with "ban" text
+        [Message("ban")]
+        public async Task Ban() => await Bot.SendTextMessageAsync(Chat, "I will ban you right now! Just kidding");
+
+        //Answer on message that satisfy provided regex expression
+        [Message("^.*?(?i)python$", regex: true)]
+        public async Task Task() => await Bot.SendTextMessageAsync(Chat, "I hate snakes");
+
+        //Answer on any update
+        [Update(InChat.All, UpdateFlag.All)]
+        public async Task Update() => await Bot.SendTextMessageAsync(Chat, "Hello");
+
+        //Answer on message that contains photo or video
+        [Message(InChat.All, MessageFlag.HasPhoto | MessageFlag.HasVideo)]
+        public async Task PhotoVideo() => await Bot.SendTextMessageAsync(Chat, "Send me more!");
     }
-
+```
 And enjoy =)
 
-Example project can be found [here](https://github.com/AleXr64/Telegram-bot-framework-examples)
+Example project can be found [here](TGBotFramework/Examples)
 
 Requires:
 
  - .net 5.0
- - [microsoft dependency injection](https://docs.microsoft.com/en-us/aspnet/core/fundamentals/dependency-injection)  (via asp.net core or hosted service template)
+ - [Microsoft Dependency Injection](https://docs.microsoft.com/en-us/aspnet/core/fundamentals/dependency-injection)  (via asp.net core or hosted service template)
 
---
+++ TGBotFramework/Examples/BotAsWorkerService/EventHandler.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading.Tasks;
+using BotFramework;
+using BotFramework.Attributes;
+using BotFramework.Setup;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using BotFramework.Enums;
+
+namespace BotAsWorkerService
+{
+    public class EventHandler:BotEventHandler
+    {
+        // Answer on "/start" command from private messages
+        [Command(InChat.Private, "start")]
+        public async Task Start() => await Bot.SendTextMessageAsync(Chat, "Hello! U started me =)");
+
+        //Answer on message with "ban" text
+        [Message("ban")]
+        public async Task Ban() => await Bot.SendTextMessageAsync(Chat, "I will ban you right now! Just kidding");
+
+        //Answer on message that satisfy provided regex expression
+        [Message("^.*?(?i)python$", regex: true)]
+        public async Task Task() => await Bot.SendTextMessageAsync(Chat, "I hate snakes");
+
+        //Answer on any update
+        [Update(InChat.All, UpdateFlag.All)]
+        public async Task Update() => await Bot.SendTextMessageAsync(Chat, "Hello");
+
+        //Answer on message that contains photo or video
+        [Message(InChat.All, MessageFlag.HasPhoto | MessageFlag.HasVideo)]
+        public async Task PhotoVideo() => await Bot.SendTextMessageAsync(Chat, "Send me more!");
+
+        //Answer on command with parameters: "/me hello"
+        [ParametrizedCommand("me", CommandParseMode.Both)]
+        public async Task Me(MeParam me)
+        {
+            var user = From.FirstName;
+            if(!string.IsNullOrEmpty(From.LastName))
+            {
+                user += $" {From.LastName}";
+            }
+            await Bot.SendTextMessageAsync(Chat, $"<code>{user} says: {me.Text}</code>", ParseMode.Html);
+        }
+    }
+
+    public class MeParam
+    {
+        public string Text;
+    }
+
+    //Custom parser for command parameters (for ParametrizedCommand)
+    public class MeParser:IRawParameterParser<MeParam>
+    {
+        public MeParam DefaultInstance() { return new MeParam();}
+
+        public bool TryGetValueByRawUpdate(Update update, out MeParam result)
+        {
+            if(update.Type != UpdateType.Message || update.Message.Type != MessageType.Text)
+            {
+                result = null;
+                return false;
+            }
+            var me = new MeParam();
+            me.Text = update.Message.Text.Replace("/me ", string.Empty);
+
+            result = me;
+            return true;
+        }
+    }
+}
