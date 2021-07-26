139b8f5bb05190d5ab261a500196365d11412a82
voed <voed@i.ua>
2021-07-26 15:20:21 +0300

    New example & some bugfixes

---
+++ TGBotFramework/Examples/MsgDumpBot/EventHandler.cs
@@ -0,0 +1,23 @@
+﻿using BotFramework;
+using BotFramework.Attributes;
+using BotFramework.Setup;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using Telegram.Bot.Types.Enums;
+
+namespace MsgDumpBot
+{
+    class EventHandler:BotEventHandler
+    {
+        [Command("msgdump", CommandParseMode.Both)]
+        public async Task MsgDump()
+        {
+            var msg = RawUpdate.Message;
+            if(msg.ReplyToMessage != null)
+            {
+                string jmsg = JsonConvert.SerializeObject(msg, Formatting.Indented);
+                await Bot.SendTextMessageAsync(Chat.Id, $"<code>{jmsg}</code>", ParseMode.Html);
+            }
+        }
+    }
+}
