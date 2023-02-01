3a1df64bbc2f04ade9f80b15af081daeae109bae
voed <voed@i.ua>
2023-02-01 18:11:25 +0200

    Initial implementation

---
+++ TGBotFramework/InteractiveTests/EventHandler.cs
@@ -0,0 +1,27 @@
+﻿using BotFramework;
+using BotFramework.Attributes;
+using Telegram.Bot;
+using System.Reflection;
+namespace InteractiveTests;
+
+public class EventHandler : BotEventHandler
+{
+    bool? AnyUpdateOk = null;
+
+    [TextMessage("OK")]
+    public async Task TestOk()
+    {
+
+    }
+    [Update]
+    public async Task AnyUpdate()
+    {
+        await Bot.SendTextMessageAsync(Chat.Id, MethodBase.GetCurrentMethod().Name);
+    }
+
+    [Message]
+    public async Task AnyMessage()
+    {
+        await Bot.SendTextMessageAsync(Chat.Id, MethodBase.GetCurrentMethod().Name);
+    }
+}
---
+++ TGBotFramework/InteractiveTests/Program.cs
@@ -0,0 +1,23 @@
+﻿using BotFramework;
+using Microsoft.Extensions.Hosting;
+
+
+
+namespace InteractiveTests
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                     {
+                         services.AddTelegramBot();
+                     });
+    }
+}
