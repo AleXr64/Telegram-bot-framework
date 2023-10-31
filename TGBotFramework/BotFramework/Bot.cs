fd3b727fb160d950bce29949f7ad8e80ff245b16
voed <voed@i.ua>
2023-10-31 23:03:15 +0200

    Custom bot api server configuration (#177)

--- TGBotFramework/BotFramework/Bot.cs
+++ TGBotFramework/BotFramework/Bot.cs
@@ -67,6 +67,11 @@ namespace BotFramework
 
             try
             {
+                var apiUrl = _config.BotApiUrl;
+                if(string.IsNullOrWhiteSpace(apiUrl))
+                    apiUrl = null;
+                var options = new TelegramBotClientOptions(_config.Token, apiUrl, _config.UseTestEnv);
+                
                 if(_config.UseSOCKS5)
                 {
                     var proxy = new HttpToSocks5Proxy(_config.SOCKS5Address, _config.SOCKS5Port, _config.SOCKS5User,
@@ -74,11 +79,11 @@ namespace BotFramework
                     var handler = new HttpClientHandler { Proxy = proxy };
                     var httpClient = new HttpClient(handler, true);
 
-                    client = new TelegramBotClient(_config.Token, httpClient);
+                    client = new TelegramBotClient(options, httpClient);
                 }
                 else
                 {
-                    client = new TelegramBotClient(_config.Token);
+                    client = new TelegramBotClient(options);
                 }
 
                 factory = new EventHandlerFactory();
